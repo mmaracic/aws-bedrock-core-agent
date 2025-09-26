92be98e06ee57fe29416fa69ba8fb715c73cdab7
mmaracic <marijo.maracic@gmail.com>
2025-09-26 02:36:29 +0200

    Basic agent running locally

--- agent-app.py
+++ agent-app.py
@@ -1,14 +1,29 @@
+import os
+
+from agents import Agent, Runner
+from agents.models.openai_chatcompletions import OpenAIChatCompletionsModel
 from bedrock_agentcore import BedrockAgentCoreApp
-from strands import Agent
+from openai import AsyncAzureOpenAI
 
 app = BedrockAgentCoreApp()
-agent = Agent()
+openai_key = os.environ["AZURE_OPENAI_API_KEY"]
+endpoint = os.environ["AZURE_ENDPOINT"]
+
+client = AsyncAzureOpenAI(
+    api_version="2025-01-01-preview",
+    api_key=openai_key,
+    azure_endpoint=endpoint
+)
+agent = Agent(
+    name="SampleAgent",
+    model=OpenAIChatCompletionsModel(model="gpt-5-nano", openai_client=client)
+)
 
 @app.entrypoint
-def invoke(payload):
+async def invoke(payload):
     user_message = payload.get("prompt", "Hello!")
-    result = agent(user_message)
-    return {"result": result.message}
+    result = await Runner.run(agent, user_message)
+    return result
 
 if __name__ == "__main__":
     app.run()
